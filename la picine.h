47220c65ffaa1284c2dd3700e28bf8d14ec564db
minkyeongg <122012752+minkyeongg@users.noreply.github.com>
2023-01-06 14:44:54 +0900

    Add files via upload

---
+++ la picine.h
@@ -0,0 +1,13 @@
+//
+//  la picine.h
+//  mingstudy
+//
+//  Created by Minkyeong Lee on 2023/01/03.
+//
+
+#ifndef la_picine_h
+#define la_picine_h
+
+#include <stdio.h>
+
+#endif /* la_picine_h */
